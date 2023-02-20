79bc1514ea54573cc090ed7f2248728dc2818aef
Gor <7495500@gmail.com>
2023-02-20 13:28:11 +0300

    Add turbo frames to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root "home#index"
+  root to: "home#index"
 
-  get 'other/index'
   get 'home/index'
+  get 'other/index'
+
+  get '/home/turbo_frame_form' => 'home#turbo_frame_form', as: 'turbo_frame_form'
+  post '/home/turbo_frame_submit' => 'home#turbo_frame_submit', as: 'turbo_frame_submit'
 end
